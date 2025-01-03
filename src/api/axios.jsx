b853154cdc48e7a2acff300adbaf3cd8041ec2f8
ErenBerkSari <erenmxc@gmail.com>
2025-01-03 17:09:30 +0300

    api config

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,61 +1,10 @@
 import axios from "axios";
-import { store } from "../redux/store";
-import { refreshAccessToken } from "../redux/slices/authSlice";
 
 const api = axios.create({
-  baseURL: "https://chess-course-project-backend-with-node-js.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-let isRefreshing = false;
-let failedQueue = [];
-
-const processQueue = (error, token = null) => {
-  failedQueue.forEach((prom) => {
-    if (error) {
-      prom.reject(error);
-    } else {
-      prom.resolve(token);
-    }
-  });
-  failedQueue = [];
-};
-
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      if (isRefreshing) {
-        return new Promise((resolve, reject) => {
-          failedQueue.push({ resolve, reject });
-        })
-          .then(() => api(originalRequest))
-          .catch((err) => Promise.reject(err));
-      }
-
-      originalRequest._retry = true;
-      isRefreshing = true;
-
-      try {
-        const action = await store.dispatch(refreshAccessToken());
-        if (refreshAccessToken.fulfilled.match(action)) {
-          isRefreshing = false;
-          processQueue(null, action.payload);
-          return api(originalRequest);
-        }
-      } catch (refreshError) {
-        processQueue(refreshError, null);
-        isRefreshing = false;
-        return Promise.reject(refreshError);
-      }
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default api;
