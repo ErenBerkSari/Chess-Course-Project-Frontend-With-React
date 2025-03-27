bdede6dabbe88b65ed531bba4f97f72be821bac8
ErenBerkSari <erenmxc@gmail.com>
2025-03-28 01:20:06 +0300

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,7 +37,11 @@ function Home() {
     dispatch(getTopUser());
     dispatch(getAllLessons());
   }, [isRegistered, navigate, dispatch]);
-
+  useEffect(() => {
+    const timer = setTimeout(() => {}, 3000); // 3 saniye yüklenme
+    console.log(timer);
+    return () => clearTimeout(timer);
+  }, []);
   const handleNavigation = () => {
     navigate("/articles");
   };
